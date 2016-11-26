29773f154ebfd2996bcdf771046430c42f6862a8
Aaron Babber <aaron.a.babber@gmail.com>
2016-11-25 20:15:05 -0600

    Moved comments in .fxml files to bottom so they would open in SceneBuilder

--- src/assignment7/ChatClient.java
+++ src/assignment7/ChatClient.java
@@ -24,7 +24,6 @@ public class ChatClient extends Observable implements Runnable{
 	try {
 		setUpNetworking();
 	} catch (Exception e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	
